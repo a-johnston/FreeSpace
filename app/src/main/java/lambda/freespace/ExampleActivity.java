ba490c7f1da0dc4c903bf696052d5dc3e542cfa7
Adam J <amj69@case.edu>
2015-05-17 19:10:12 -0400

    Initial commit

---
+++ app/src/main/java/lambda/freespace/ExampleActivity.java
@@ -0,0 +1,14 @@
+package lambda.freespace;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ExampleActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
